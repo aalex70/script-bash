86ce783077efb29a31c609f2c985cffd36f653e1
aalex70 <ale.alfonsi@gmail.com>
2019-11-01 22:36:06 +0100

    Add files via upload

---
+++ rsync-smartphone.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+# rsync photos for Android devices connected in MTP mode
+
+# Check for arguments before running script:
+
+if  [ -z "$1"  ]; then
+	echo
+	echo "  $(basename $0) Error: No argument. Enter device name"
+	echo
+	echo "  Example: $(basename $0) Nexus-5x"
+	echo
+	exit 1
+fi
+
+DEVICE=$1
+ORIG=$(mount | grep gvfs | awk '{ print $3 }')
+DEST="/home/alex/${DEVICE}"
+[ ! -d $DEST ] && mkdir -p $DEST
+cd $ORIG
+cd *
+CWD=$(pwd)
+
+while read LINE; do
+	cd "$CWD"
+	mkdir -p "${DEST}/${LINE}"
+	cd "$CWD/${LINE}"
+	pwd
+	# exclude ".Statuses" WhapsApp directory because rsync returns read errors failed verification
+	rsync -av --exclude '*.Statuses*' . "${DEST}/${LINE}"
+done < <(find . -maxdepth 3 -name 'WhatsApp' -o \
+	-name 'DCIM' -o \
+	-name 'Pictures' -type d)
+
+# vim: ts=2 sw=2 noet ai nohls
