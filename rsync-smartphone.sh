df7a6708560e716a7295ada927fe218eb0606128
Alessandro Alfonsi <ale.alfonsi@gmail.com>
2026-02-03 17:53:21 +0100

    added specific backup for whatapps

--- rsync-smartphone.sh
+++ rsync-smartphone.sh
@@ -42,7 +42,8 @@ while read LINE; do
 	pwd
 	# exclude ".Statuses" WhapsApp directory because rsync returns read errors failed verification
 	rsync -av --exclude '*.Statuses*' --exclude '.thumbnails' --exclude '.aux' --exclude '*.db.crypt*' . "${DEST}/${LINE}"
-done < <(find . -maxdepth 3 -name 'WhatsApp' -o \
+done < <(find . -maxdepth 3 \
+	-name 'WhatsApp' -o \
 	-name 'DCIM' -o \
 	-name 'Music' -o \
 	-name 'Screenshots' -o \
@@ -55,8 +56,30 @@ done < <(find . -maxdepth 3 -name 'WhatsApp' -o \
 	-name 'mycar' -o \
 	-name 'Telegram' -o \
 	-name 'Threema' -o \
+	-name 'app_backup' -o \
 	-name 'Pictures' -type d)
 
-# togliere mycar, kpx, Threema e Telegram se non sono installati
+# remove directory mycar, kpx, Threema, app_backup and Telegram if they are not installed
+         
+## added specific backup for whatapps
+#
+cd $ORIG
+cd mtp*
+CWD=$(pwd)
+
+while read WA_DIR; do
+	cd "$CWD"
+	mkdir -p "${DEST}/${WA_DIR}"
+	cd "$CWD/${WA_DIR}"
+	pwd
+	rsync -av --exclude '*Private*' --exclude '*Sent*' . "${DEST}/${WA_DIR}"
+done < <(find ./Memoria\ condivisa\ interna/Android/media/com.whatsapp/WhatsApp/Media/ -maxdepth 1 \
+		-name 'WhatsApp*Images' -o \
+		-name 'WhatsApp*Animated*Gifs' -o \
+		-name 'WhatsApp*Video' -o \
+		-name 'WhatsApp*Audio' -o \
+		-name 'WhatsApp*Documents' -o \
+		-name 'WhatsApp*Voice*Notes' -o \
+		-name 'WhatsApp*Stickers' -type d)
 
 # vim: ts=2 sw=2 noet ai nohls
